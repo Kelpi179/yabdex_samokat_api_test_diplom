464ab933f2b6fe6107de6e1fafcf3b2eeb6fc503
Kelpi179 <141927927+Kelpi179@users.noreply.github.com>
2023-09-05 16:15:03 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://f184b33b-078e-4de8-ba12-50ad337e725a.serverhub.praktikum-services.ru/"
+CREATE_ORDER = "api/v1/orders"
+REQUEST_ORDER = "api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,20 @@
+header_content = {
+        "Content-Type": "application/json"
+}
+order_content = {
+    "firstName": "Иван",
+    "lastName": "Романов",
+    "address": "Арбат",
+    "metroStation": 4,
+    "phone": "+7 999 999 99 99",
+    "rentTime": 2,
+    "deliveryDate": "2023-10-10",
+    "comment": "Не звонить",
+    "color": [
+        "BLACK"
+    ]
+}
+
+params_get = {
+    "t": ""
+}
